296850a08f8648553705d13af47bd0996cd1c0e6
hamdi-karim <k.hamdi1995@gmail.com>
2024-08-20 11:52:01 +0100

    applying expanding rotation animation with Framer Motion library

--- src/components/ChallengeItem.jsx
+++ src/components/ChallengeItem.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { useContext } from "react";
 
 import { ChallengesContext } from "../store/challenges-context.jsx";
@@ -42,13 +43,19 @@ export default function ChallengeItem({
             </p>
           </div>
         </header>
-        <div
-          className={`challenge-item-details ${isExpanded ? "expanded" : ""}`}
-        >
+        <div className="challenge-item-details">
           <p>
             <button onClick={onViewDetails}>
               View Details{" "}
-              <span className="challenge-item-details-icon">&#9650;</span>
+              <motion.span
+                className="challenge-item-details-icon"
+                animate={{ rotate: isExpanded ? 180 : 0 }}
+                transition={{
+                  duration: 0.3,
+                }}
+              >
+                &#9650;
+              </motion.span>
             </button>
           </p>
 
